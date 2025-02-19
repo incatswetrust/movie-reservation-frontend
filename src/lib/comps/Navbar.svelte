10755e50d5b92bc47d3376ac548bce4b71a24508
Oleksii <cmmxr777@gmail.com>
2025-02-19 22:25:12 +0200

    added some cinemas functional

--- src/lib/comps/Navbar.svelte
+++ src/lib/comps/Navbar.svelte
@@ -65,8 +65,8 @@
     {#if $user.isSuccess && $user.data!== null && $user.data!== undefined}
     <button on:click={logout} class="items-center text-cyan-300 hover:text-cyan-100 transition-colors">
       <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="ml-3" viewBox="0 0 16 16">
-        <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0z"/>
-        <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708z"/>
+        <path d="M8.5 10c-.276 0-.5-.448-.5-1s.224-1 .5-1 .5.448.5 1-.224 1-.5 1"/>
+  <path d="M10.828.122A.5.5 0 0 1 11 .5V1h.5A1.5 1.5 0 0 1 13 2.5V15h1.5a.5.5 0 0 1 0 1h-13a.5.5 0 0 1 0-1H3V1.5a.5.5 0 0 1 .43-.495l7-1a.5.5 0 0 1 .398.117M11.5 2H11v13h1V2.5a.5.5 0 0 0-.5-.5M4 1.934V15h6V1.077z"/>
       </svg>
     </button>
     {/if}
--- src/routes/cinemas/+page.svelte
+++ src/routes/cinemas/+page.svelte
@@ -1,5 +1,22 @@
-<script>
+<script lang="ts">
 	import Cinemas from "$lib/comps/cinemas/Cinemas.svelte";
+	import {createMutation, createQuery, useQueryClient} from "@tanstack/svelte-query";
+    import  type {AxiosResponse} from "axios";
+	import { UserRole, type UserReadDto } from "../../Api";
+    import { api } from "../../Module";
+	import CinemaTable from "$lib/comps/cinemas/CinemaTable.svelte";
 
+	const user = createQuery<UserReadDto>({
+        queryKey: ['user'],
+        queryFn: async () => {
+            const response: AxiosResponse<UserReadDto> = await api.auth.authStatusList();
+                return response.data;
+        }
+    });
 </script>
-<Cinemas/>
+
+{#if $user.isSuccess && $user.data!==undefined && $user.data!==null && $user.data.role === UserRole.Value1}
+	<CinemaTable/>
+{:else}
+	<Cinemas/>
+{/if}
