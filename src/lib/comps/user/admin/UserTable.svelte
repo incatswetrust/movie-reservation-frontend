cdf85173330d78e091295ddf1db494ad35658f02
Oleksii <cmmxr777@gmail.com>
2025-02-19 18:01:10 +0200

    created deleting user

--- src/lib/comps/user/admin/UserTable.svelte
+++ src/lib/comps/user/admin/UserTable.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
-    import {createQuery} from "@tanstack/svelte-query";
+    import {createMutation, createQuery, useQueryClient} from "@tanstack/svelte-query";
     import type {AxiosResponse} from "axios";
     import { api } from "../../../../Module";
 	import {type UserReadDto } from "../../../../Api";
+	import { applyAction } from "$app/forms";
+
+
+    const client = useQueryClient();
 
     const users = createQuery<UserReadDto[]>({
         queryKey: ['users'],
@@ -11,6 +15,20 @@
                 return response.data;
         }
     });
+
+    const deleteMutation = createMutation({
+        mutationFn: async (id: number) =>{
+            await api.users.usersDelete(id);
+        },
+        onSuccess: () =>{
+            client.invalidateQueries({queryKey: ['users']})
+        }
+    })
+
+    function Delete(id: number|undefined){
+        if(id!==undefined)
+            $deleteMutation.mutate(id);
+    }
 
 </script>
 
@@ -55,15 +73,15 @@
             {user.role} <!--Change to user bookings-->
           </td>
           <td class="px-4 py-2 whitespace-nowrap">
-            <a
-              href="#"
+            <button
+            on:click={(() => Delete(user.id))}
               class="inline-block rounded-sm bg-transperent px-4 py-2 text-xs font-medium text-fuchsia-600 hover:text-fuchsia-500 transition-colors"
             >
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
                 <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
                 <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
               </svg>
-            </a>
+            </button>
           </td>
         </tr>
         {/each}  
