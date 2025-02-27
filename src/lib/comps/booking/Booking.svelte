1b5cb75799bde4b2ab1744a14bf373abad503f1d
Oleksii <cmmxr777@gmail.com>
2025-02-27 20:31:30 +0200

    Added cinema plan to confirming booking

--- src/lib/comps/booking/Booking.svelte
+++ src/lib/comps/booking/Booking.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
 	import { createQuery } from "@tanstack/svelte-query";
-	import { type SeatReadDto, type CinemaReadDto, type HallReadDto, type ShowtimeReadDto } from "../../../Api";
+	import { type SeatReadDto, type CinemaReadDto, type HallReadDto, type ShowtimeReadDto, type MovieReadDto } from "../../../Api";
 	import { api } from "../../../Module";
 	import type { AxiosResponse } from "axios";
+	import { FormatParser } from "$lib/tools/FormatParser";
 
     export let Id: number;
 
@@ -39,6 +40,14 @@
         }
     });
 
+    const movie = createQuery<MovieReadDto>({
+        queryKey: ['movie'],
+        queryFn: async()=> {
+            const response: AxiosResponse<MovieReadDto> = await api.movies.moviesDetail($showtime.data?.movieId!== undefined ? $showtime.data?.movieId: 0);
+            return response.data;
+        }
+    })
+
 
     $: groupedByRow = ($seats.data || []).reduce((acc, seat) => {
         if (seat.rowLabel!==undefined && seat.rowLabel!==null && !acc[seat.rowLabel]) {
@@ -49,5 +58,74 @@
     return acc;
   }, {} as Record<string, SeatReadDto[]>);
 
-  
+  // Храним выбранные места (id)
+  let selectedSeatIds: number[] = [];
+
+ 
+  function toggleSeat(seat: SeatReadDto) {
+    if (seat.isReserved) {
+      return;
+    }
+
+    if (seat.id == null) return;
+    if (selectedSeatIds.includes(seat.id)) {
+      selectedSeatIds = selectedSeatIds.filter(id => id !== seat.id);
+    } else {
+      selectedSeatIds = [...selectedSeatIds, seat.id];
+    }
+  }
+
+
 </script>
+
+<div class="border border-cyan-500 rounded p-4 text-cyan-300 container px-5 mx-auto overflow-y-auto">
+
+    <h2 class="text-xl mb-4">{$movie.data?.title}</h2>
+    <h3 class="text-l mb-4">{$cinema.data?.name} | {$hall.data?.name} | {FormatParser.formatDateTime($showtime.data?.startTime)}</h3>
+    <h3 class="text-l mb-4">{$cinema.data?.address}</h3>
+
+    <!--Screen-->
+        <div class="py-3 flex items-center text-s text-cyan-600 uppercase before:flex-1 before:border-2 before:border-cyan-600 before:me-6 after:flex-1 after:border-2 after:border-cyan-600 after:ms-6">Screen</div>
+   
+
+        <!--Hall plan-->
+        {#each Object.keys(groupedByRow) as row}
+  <div class="flex items-center mb-2">
+    
+    <div class="mr-4 text-cyan-500 font-bold w-6 flex justify-end">
+      {row}
+    </div>
+
+    
+    <div class="flex-1 flex justify-between">
+      {#each groupedByRow[row] as seat}
+       
+        <button
+          class="
+            border border-cyan-500
+            w-8 h-8
+            flex items-center justify-center
+            transition-colors
+          "
+          class:text-cyan-300={seat.id!==undefined && !seat.isReserved && !selectedSeatIds.includes(seat.id)}
+          class:cursor-pointer={!seat.isReserved}
+          
+          on:click={() => toggleSeat(seat)}
+          
+          class:bg-fuchsia-600={seat.isReserved}
+          class:text-black={(seat.isReserved || (seat.id!==undefined && !seat.isReserved && selectedSeatIds.includes(seat.id)))}
+          class:cursor-not-allowed={seat.isReserved}
+          class:bg-cyan-600={seat.id!==undefined && !seat.isReserved && selectedSeatIds.includes(seat.id)}
+          
+        >
+          {seat.seatNumber}
+        </button>
+      {/each}
+    </div>
+    <div class="mr-4 text-cyan-500 font-bold w-6 flex justify-end">
+        {row}
+      </div>
+  </div>
+{/each}
+</div>
+
