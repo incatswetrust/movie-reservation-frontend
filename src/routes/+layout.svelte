b267b60cfce529b1e639bf2abf53075df2156c8d
Oleksii <cmmxr777@gmail.com>
2025-03-05 21:04:08 +0200

    changed title on tab

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -15,8 +15,12 @@ import '../app.css';
   });
 </script>
 
+<svelte:head>
+  <title>Movie reservation system</title>
+</svelte:head>
 
 <QueryClientProvider client={queryClient}>
+ 
 	<Navbar/>
   <!-- <section class="bg-black min-h-[100vh] bg-no-repeat bg-cover bg-center" style="background-image: url('/CyanLayer.svg');"> -->
    <section class="bg-black bg-cover bg-center min-h-[100vh]">
