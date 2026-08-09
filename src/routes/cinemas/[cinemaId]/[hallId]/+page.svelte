3b18950369ae68d227ac2d6152dfa087aaa39cdb
hidayat-cmd <SinagaHidayat@duck.com>
2026-08-09 09:34:42 +0300

    Redesign hall detail page with image gallery and admin upload/delete

--- src/routes/cinemas/[cinemaId]/[hallId]/+page.svelte
+++ src/routes/cinemas/[cinemaId]/[hallId]/+page.svelte
@@ -1,21 +1,8 @@
 <script lang="ts">
-    import type { PageData } from './$types'
-	import CinemaViewForAdmin from '$lib/comps/cinemas/CinemaViewForAdmin.svelte';
-	import HallViewForAdmin from '$lib/comps/halls/HallViewForAdmin.svelte';
-	import { createQuery } from '@tanstack/svelte-query';
-	import { UserRole, type UserReadDto } from '../../../../Api';
-	import type { AxiosResponse } from 'axios';
-	import { api } from '../../../../Module';
-	import ShowtimesByHall from '$lib/comps/showtimes/ShowtimesByHall.svelte';
-    export let data: PageData;
-    const user = createQuery<UserReadDto>({
-        queryKey: ['user'],
-        queryFn: async () => {
-            const response: AxiosResponse<UserReadDto> = await api.auth.authStatusList();
-                return response.data;
-        }
-    });
+	import type { PageData } from './$types';
+	import HallDetail from '$lib/comps/halls/HallDetail.svelte';
+
+	export let data: PageData;
 </script>
-{#if $user.isSuccess && $user.data!==undefined && $user.data!==null && $user.data.role === UserRole.Value1}
-    <HallViewForAdmin Id={data.id}/>
-{/if}
+
+<HallDetail Id={data.id} />
