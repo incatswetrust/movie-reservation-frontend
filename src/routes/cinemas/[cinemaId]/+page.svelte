7c85f1058daabe6bc51233ed9b86b876ba0220a4
hidayat-cmd <SinagaHidayat@duck.com>
2026-08-09 22:02:42 +0300

    Redesign cinema detail page with unified halls grid, replacing admin/regular split

--- src/routes/cinemas/[cinemaId]/+page.svelte
+++ src/routes/cinemas/[cinemaId]/+page.svelte
@@ -1,14 +1,8 @@
 <script lang="ts">
-    import type { PageData } from './$types'
-	import CinemaViewForAdmin from '$lib/comps/cinemas/CinemaViewForAdmin.svelte';
-	import CinemaViewForRegular from '$lib/comps/cinemas/CinemaViewForRegular.svelte';
-	import { auth } from '$lib/stores/auth';
+	import type { PageData } from './$types';
+	import CinemaDetail from '$lib/comps/cinemas/CinemaDetail.svelte';
 
-    export let data: PageData;
+	export let data: PageData;
 </script>
 
-{#if $auth.isAdmin}
-    <CinemaViewForAdmin Id={data.id}/>
-{:else}
-    <CinemaViewForRegular Id={data.id}/>
-{/if}
+<CinemaDetail Id={data.id} />
