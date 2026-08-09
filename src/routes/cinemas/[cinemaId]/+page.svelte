bccfd07c036348f498f26ca1844a587e7b5ea76a
hidayat-cmd <SinagaHidayat@duck.com>
2026-08-09 09:37:40 +0300

    Apply admin route guards and fix cinema detail role check

--- src/routes/cinemas/[cinemaId]/+page.svelte
+++ src/routes/cinemas/[cinemaId]/+page.svelte
@@ -1,22 +1,14 @@
 <script lang="ts">
     import type { PageData } from './$types'
 	import CinemaViewForAdmin from '$lib/comps/cinemas/CinemaViewForAdmin.svelte';
-	import { createQuery } from '@tanstack/svelte-query';
-	import { UserRole, type UserReadDto } from '../../../Api';
-	import type { AxiosResponse } from 'axios';
-	import { api } from '../../../Module';
-	import Cinemas from '$lib/comps/cinemas/Cinemas.svelte';
 	import CinemaViewForRegular from '$lib/comps/cinemas/CinemaViewForRegular.svelte';
-    export let data: PageData;
+	import { auth } from '$lib/stores/auth';
 
-    const user = createQuery<UserReadDto>({
-        queryKey: ['user'],
-        queryFn: async () => {
-            const response: AxiosResponse<UserReadDto> = await api.auth.authStatusList();
-                return response.data;
-        }
-    });
+    export let data: PageData;
 </script>
-{#if $user.isSuccess && $user.data!==undefined && $user.data!==null && $user.data.role === UserRole.Value1}
+
+{#if $auth.isAdmin}
     <CinemaViewForAdmin Id={data.id}/>
+{:else}
+    <CinemaViewForRegular Id={data.id}/>
 {/if}
